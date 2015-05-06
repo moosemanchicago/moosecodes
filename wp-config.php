21426e9a84fab60eccb4512124c5831e15d0dbe0
Mustafa Ishaq <moose@Mustafas-MacBook-Pro.local>
2015-05-05 23:26:00 -0500

    added back files from test

---
+++ wp-config.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, and ABSPATH. You can find more information by visiting
+ * {@link http://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'moosecodes');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         ')JM368g@RE[H}|?Dq^$q:xX/4&5>&qm,fj<olq[K%HxJW] MUR-UNaT,arG{i3MK');
+define('SECURE_AUTH_KEY',  ']SB]U*hb.);Z]0D<~_i(swj}mt!EydG1KjwcmOm].dLm3rPN)/(j8*y*PZ$[IN=6');
+define('LOGGED_IN_KEY',    'N0;Q+w.g~9g$YUi@KI</Dh{?,0Lsi!NpClG0Tp-8_jf28U68<c=(++1o)yGlEB)a');
+define('NONCE_KEY',        'bM;T*KeC0?_z1)g/;L8}$-{kiWwz5=6_Yc|-;}af-wQr=Iwi[d-~gy)z|#D%A#Ek');
+define('AUTH_SALT',        '9r@Gn|i*h[:-WS9Bq12B-#Q$k|P6xG:-sDL/{=v);>~?Kc#$af?-5bdzNIdrD&FH');
+define('SECURE_AUTH_SALT', 'm<(qM&n{u/5dYT4p_lR|Jzuw-+F@;TU:OA,kQk$(b;SoYt|mTD.X~[sh,sG*i!|-');
+define('LOGGED_IN_SALT',   'CZ{o)>)dJqt87-&{Ah<zG^2nQ`*:$nrC>DQzy(AD&&t1}]h%R9&Es6E-x[]R0u@]');
+define('NONCE_SALT',       'YCvzAGj7xh|XJ!D*~A|aPAx-e&lDTKHjOTF>)8`1@s[W1k-_q;0e/]r]Ge8 `zJ ');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/naked-wordpress-master/header.php
@@ -0,0 +1,122 @@
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* This template will be called by all other template files to begin 
+	/* rendering the page and display the header/nav
+	/*-----------------------------------------------------------------------------------*/
+?>
+<!DOCTYPE html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>
+	<?php bloginfo('name'); // show the blog name, from settings ?> | 
+	<?php is_front_page() ? bloginfo('description') : wp_title(''); // if we're on the home page, show the description, from the site's settings - otherwise, show the title of the post or page ?>
+</title>
+
+<link rel="stylesheet" type="text/css" href="styles/foundation.css" />
+<link rel="stylesheet" type="text/css" href="styles/normalize.css" />
+<link rel="stylesheet" type="text/css" href="styles/css.css">
+<link href='http://fonts.googleapis.com/css?family=Lato:100,400' rel='stylesheet' type='text/css'>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<script src="js/vendor/modernizr.js"></script>
+
+<?php // We are loading our theme directory style.css by queuing scripts in our functions.php file, 
+	// so if you want to load other stylesheets,
+	// I would load them with an @import call in your style.css
+?>
+
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+<?php wp_head(); 
+// This fxn allows plugins, and Wordpress itself, to insert themselves/scripts/css/files
+// (right here) into the head of your website. 
+// Removing this fxn call will disable all kinds of plugins and Wordpress default insertions. 
+// Move it if you like, but I would keep it around.
+?>
+
+</head>
+
+<body 
+	<?php body_class(); 
+	// This will display a class specific to whatever is being loaded by Wordpress
+	// i.e. on a home page, it will return [class="home"]
+	// on a single post, it will return [class="single postid-{ID}"]
+	// and the list goes on. Look it up if you want more.
+	?>
+>
+
+
+
+<div class="main-fluid"><!-- start the page containter -->
+
+
+
+    <!-- left half -->
+	<section id="leftSection" class="leftSide">
+
+        <div class="row top-half">
+            <figure id="left-image" class="columns small-12 left-image"></figure>
+            <!-- <div class="vignette"></div> -->
+        </div>
+
+        <div class="row bottom-half"></div>
+        
+    </section>
+    <!-- end left half -->
+
+
+    <!-- right half -->
+    <section id="rightSection" class="">
+
+        <div class="row top-half"><!-- <div class="vignette"></div> --></div>
+
+            <!-- <canvas id="myCanvas" height="300" width="300"></canvas> -->
+            <article class="row middle-section business-card">
+
+                <header class="business-card-header columns small-12">
+                    <h1>Mustafa Ishaq</h1>
+                    <h3>Web Developer</h3>
+                    <h3>Chicago , Illinois</h3>
+                    <h3>moose @ moosecodes . com</h3>
+                    <!-- <h3>312.810.2010</h3> -->
+                </header>
+
+                <footer class="business-card-footer columns small-12">
+                    <!-- <nav>
+                        <span class="columns small-4"><a href="">Work</a></span>
+                        <span class="columns small-4"><a href="">Experience</a></span>
+                        <span class="columns small-4"><a href="">Contact</a></span>
+                        <span class="columns small-2"><a href="">Recreation</a></span>
+                    </nav> -->
+                    <?php wp_nav_menu( array( 'theme_location' => 'secondary_navigation' ) ); // Display the user-defined menu in Appearance > Menus ?>
+                </footer>
+
+            </article>
+
+            <div class="row bottom-half"></div>
+
+    </section>
+    <!-- end right half -->
+
+
+    <!-- fastener sits on top -->
+    <div id="fastener" class="logo">
+        <div class="loader-bg" id="loader-bg"></div>
+        <i class="i i-logo">
+            <svg xmlns="http://www.w3.org/2000/svg" id="logo" version="1.1" x="0" y="0" width="44" height="32" viewBox="0 0 44 32" enable-background="new 0 0 44 32" xml:space="preserve">
+                <clipPath id="logo-clippath">
+                    <rect x="1" y="1" width="42" height="30"></rect>
+                </clipPath>
+                    <rect id="logo-fill-1" x="1" y="1" clip-path="url(#logo-clippath)" width="42" height="30" style="fill: rgb(81, 88, 105);"></rect>
+                    <rect id="logo-fill-2" x="1" y="1" clip-path="url(#logo-clippath)" width="42" height="30" style="fill: rgb(81, 88, 105);"></rect>
+                    <path id="logo-mask" d="M0 0v32h44V0H0zM8 30c-3.3 0-6-2.7-6-6s2.7-6 6-6 6 2.7 6 6S11.3 30 8 30zM8 14c-3.3 0-6-2.7-6-6s2.7-6 6-6 6 2.7 6 6S11.3 14 8 14zM28 30h-8V2h8V30zM42 30h-8V2h8V30z">
+                </path>
+            </svg>
+        </i>
+    </div>
+    <!-- end fastener -->
